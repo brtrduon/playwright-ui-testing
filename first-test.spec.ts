0cc8e90330f65b4ee075e02dddd5ed360cec1d3d
Brian Duong <brianduong@Brians-MacBook-Air.local>
2026-08-09 14:43:09 -0700

    added first-test.spec.ts

---
+++ first-test.spec.ts
@@ -0,0 +1,13 @@
+import {test} from '@playwright/test'
+
+test('this is a first test', ({page}) => {
+    page.goto('https://playground.bondaracademy.com/')
+})
+
+test('this is a first test 2', () => {
+    
+})
+
+test('this is a first test 3', () => {
+    
+})

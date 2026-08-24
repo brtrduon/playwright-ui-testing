ea45568f604df6751be9cb978a60a66fdf9ddfc0
Brian D <brcduong@gmail.com>
2026-08-24 14:51:23 -0700

    added hooks

--- tests/first-test.spec.ts
+++ tests/first-test.spec.ts
@@ -1,7 +1,35 @@
 import { test } from '@playwright/test'
 
-test('this is a first test', async ({ page }) => {
+test.beforeEach(async({page}) => {
     await page.goto('https://playground.bondaracademy.com/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
+})
+
+test.describe('suite 1', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Forms').click()
+    })
+
+    test('this is a first test', async ({ page }) => {
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('this is a first test to date picker', async ({ page }) => {
+        await page.getByText('Datepicker').click()
+    })
+
+})
+
+
+test.describe('suite 2', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Charts').click()
+    })
+
+    test('this is a first test', async ({ page }) => {
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('this is a first test to date picker', async ({ page }) => {
+        await page.getByText('Datepicker').click()
+    })
 })
